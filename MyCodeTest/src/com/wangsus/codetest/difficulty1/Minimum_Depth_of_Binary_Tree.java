09bb00f4457427905b6596eb7021c4bf93e26ed6
Su Wang <wangsu@wangsus.com>
2014-04-20 23:11:42 -0400

    Minimum_Depth_of_Binary_Tree  Wrong
    Wrong Answer
    
    Input: {1,2,#,3,#,4,#,5}
    Output: 2
    Expected: 5
    
    Need to understand what question means!!!

--- MyCodeTest/src/com/wangsus/codetest/difficulty1/Minimum_Depth_of_Binary_Tree.java
+++ MyCodeTest/src/com/wangsus/codetest/difficulty1/Minimum_Depth_of_Binary_Tree.java
@@ -35,6 +35,10 @@ public class Minimum_Depth_of_Binary_Tree {
 	    public int minDepth(TreeNode root) {
 	    	if(root==null){
 	    		return 0;
+	    	}else if(root.left==null&&root.right==null){
+	    		return 1;
+	    	}else if(root.left==null||root.right==null){
+	    		return 2;
 	    	}else{
 	    		return 1+Math.min(minDepth(root.left), minDepth(root.right));
 	    	}	        
