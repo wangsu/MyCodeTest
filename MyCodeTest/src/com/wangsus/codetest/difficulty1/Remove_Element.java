115baecafb51dea770a9dc4d4365c9c494cd83aa
Su Wang <wangsu@wangsus.com>
2014-04-22 03:37:28 -0400

    Remove_Element Accept
    look note....

--- MyCodeTest/src/com/wangsus/codetest/difficulty1/Remove_Element.java
+++ MyCodeTest/src/com/wangsus/codetest/difficulty1/Remove_Element.java
@@ -19,11 +19,45 @@ public class Remove_Element {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Solution s = new Solution();
+		int[] a = { 2, 2, 3 };
+		System.out.println(s.removeElement(a, 2));
 
+		int[] b = { 3, 3 };
+		System.out.println(s.removeElement(b, 3));
+
+		int[] c = { 2, 3, 3 };
+		System.out.println(s.removeElement(c, 3));
+
+		int[] d = { 0, 4, 4, 0, 4, 4, 4, 0, 2 };
+		System.out.println(s.removeElement(d, 4));
+
+		int[] e = { 2, 1, 3, 0, 1, 1, 0, 3, 3, 1, 3 };
+
+		System.out.println(s.removeElement(e, 3));
+
+		int[] f = { 1, 2, 3, 4 };
+
+		System.out.println(s.removeElement(f, 1));
+
+		int[] g = { 1, 2, 0, 0, 1, 2, 1, 1, 4, 0, 4, 0 };
+		System.out.println(s.removeElement(g, 1));
+
+		int[] h = { 0, 3, 1, 1, 0, 1, 3, 0, 3, 3, 1, 1 };
+		System.out.println(s.removeElement(h, 1));
 	}
-	public class Solution {
-	    public int removeElement(int[] A, int elem) {
-	        return 0;
-	    }
+
+	public static class Solution {
+		public int removeElement(int[] A, int elem) {
+			int i=0, j=0;
+	        while(j<A.length){
+	            if(A[j]!=elem){
+	                A[i]=A[j];
+	                i++; 
+	            }
+	            j++;
+	        }
+	        return i;
+		}
 	}
 }
