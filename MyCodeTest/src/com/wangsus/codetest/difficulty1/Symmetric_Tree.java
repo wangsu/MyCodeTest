ed694798148e058fa526f10f82ec099e7f17f5c3
Su Wang <wangsu10@gmail.com>
2014-04-22 11:00:31 -0400

    Symmetric_Tree better layout
    rollback the desc

--- MyCodeTest/src/com/wangsus/codetest/difficulty1/Symmetric_Tree.java
+++ MyCodeTest/src/com/wangsus/codetest/difficulty1/Symmetric_Tree.java
@@ -11,14 +11,24 @@ import com.wangsus.codetest.datastructure.TreeNode;
 /**
  * @author Su
  *
- *         Given a binary tree, check whether it is a mirror of itself (ie,
- *         symmetric around its center).
- * 
- *         For example, this binary tree is symmetric:
- * 
- *         1 / \ 2 2 / \ / \ 3 4 4 3 But the following is not: 1 / \ 2 2 \ \ 3 3
- *         Note: Bonus points if you could solve it both recursively and
- *         iteratively.
+ *Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+
  */
 public class Symmetric_Tree {
 
