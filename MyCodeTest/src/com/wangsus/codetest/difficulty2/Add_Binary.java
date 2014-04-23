052ba36358c652ddd2b75964facaa10ffc8b4544
Su Wang <wangsu@wangsus.com>
2014-04-22 23:45:19 -0400

    Add_Binary Fail
    Extra Long String
    Input:
    "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
    "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"
    Output: "0"
    Expected:
    "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000"

--- MyCodeTest/src/com/wangsus/codetest/difficulty2/Add_Binary.java
+++ MyCodeTest/src/com/wangsus/codetest/difficulty2/Add_Binary.java
@@ -19,12 +19,57 @@ public class Add_Binary {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String thisA = "0";
+		String thisB = "1";
+		Solution s = new Solution();
+		System.out.println(s.addBinary(thisA,thisB));
 	}
-	public class Solution {
+	public static class Solution {
 	    public String addBinary(String a, String b) {
-	        return "";
+	    	//need ask the length , need consider overflow. seems cannot convert to int for extra long string.
+	    	long aa = 0;
+	    	long bb = 0;
+	    	try{
+	    		aa = Long.parseLong(a);	    		
+	    	}catch(Exception e){	    		
+//	    		System.out.println("Something Wrong.");
+	    	}
+	    	try{
+	    		bb = Long.parseLong(b);
+	    	}catch(Exception e){
+//	    		System.out.println("Something Wrong.");
+	    	}
+	    	long total = aa+bb;
+	    	System.out.println("aa:"+aa+":bb:"+bb+":total:"+total);
+	    	String totalStr = ""+total;
+	    	boolean up = false;
+	    	StringBuilder sb = new StringBuilder();
+	    	sb.append(totalStr);
+	    	//remember string is from left to right, int is reverse.
+	    	for(int i=sb.length()-1;i>=0;i--){
+	    		if(up){
+		    		if(sb.charAt(i)=='0'){
+		    			sb.setCharAt(i, '1');
+		    			up = false;
+		    		}else if(sb.charAt(i)=='1'){
+		    			sb.setCharAt(i, '0');
+		    			up = true;
+		    		}else if(sb.charAt(i)=='2'){
+		    			sb.setCharAt(i, '1');
+		    			up = true;
+		    		}
+	    		}else{
+	    			if(sb.charAt(i)=='2'){
+		    			sb.setCharAt(i, '0');
+		    			up = true;
+		    		}
+	    		}
+	    	}
+	    	if(up){
+	    		return "1"+sb.toString();
+	    	}else{
+	    		return sb.toString();
+	    	}	        
 	    }
 	}
 }
