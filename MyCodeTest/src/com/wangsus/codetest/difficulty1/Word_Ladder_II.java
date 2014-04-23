a2ccc8fd323583d09b0ea8f946309c4789e7717d
Su Wang <wangsu@wangsus.com>
2014-04-22 23:10:29 -0400

    Word_Ladder_II unfinished
    will do it later.
    frequency 1
    difficulty should be 3 or 4.

--- MyCodeTest/src/com/wangsus/codetest/difficulty1/Word_Ladder_II.java
+++ MyCodeTest/src/com/wangsus/codetest/difficulty1/Word_Ladder_II.java
@@ -4,7 +4,11 @@
 package com.wangsus.codetest.difficulty1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Su
@@ -27,6 +31,9 @@ Return
 Note:
 All words have the same length.
 All words contain only lowercase alphabetic characters.
+
+frequency 1 
+difficulty should be 3 or 4.
  */
 public class Word_Ladder_II {
 
@@ -44,8 +51,46 @@ public class Word_Ladder_II {
 			//question?
 			//could repeat? should not. otherwise no meaning.
 			//
+			if(!dict.contains(end)){
+				return null;
+			}
+			ArrayList<ArrayList<String>> ret = new ArrayList<ArrayList<String>>();
+			ArrayList<HashSet<String>> listSet = new ArrayList<HashSet<String>>();
+			Iterator<String> itr = dict.iterator();
+			while(itr.hasNext()){
+				String dictStr = itr.next();
+				if(diffOneLetter(start,dictStr)){
+					ArrayList<String> stringList = new ArrayList<String>();
+					stringList.add(start);
+					stringList.add(dictStr);
+					ret.add(stringList);
+				}
+			}
 			
-			return null;
+			
+			return ret;
+		}
+		
+		private boolean diffOneLetter(String str1, String str2){
+			int diff = 0;
+			if(str1!=null&&str2!=null){
+				if(str1.length()!=str2.length()){
+					return false;
+				}else{
+					for(int i=0;i<str1.length();i++){
+						if(str1.charAt(i)!=str2.charAt(i)){
+							diff++;
+						}
+					}
+				}
+			}else{
+				return false;
+			}
+			if(diff==1){
+				return true;
+			}else{
+				return false;
+			}
 		}
 	}
 }
