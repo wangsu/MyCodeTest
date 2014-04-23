7b3b7b654c818e75cf85aa0016c716440ebd3cec
Su Wang <wangsu@wangsus.com>
2014-04-23 00:18:37 -0400

    Add Binary  Accept
    Character.getNumericValue

--- MyCodeTest/src/com/wangsus/codetest/difficulty2/Add_Binary.java
+++ MyCodeTest/src/com/wangsus/codetest/difficulty2/Add_Binary.java
@@ -27,49 +27,100 @@ public class Add_Binary {
 	public static class Solution {
 	    public String addBinary(String a, String b) {
 	    	//need ask the length , need consider overflow. seems cannot convert to int for extra long string.
-	    	long aa = 0;
-	    	long bb = 0;
-	    	try{
-	    		aa = Long.parseLong(a);	    		
-	    	}catch(Exception e){	    		
-//	    		System.out.println("Something Wrong.");
+	    	//remember string is from left to right, int is reverse.	    	
+	    	if(a==null){
+	    		return b;
 	    	}
-	    	try{
-	    		bb = Long.parseLong(b);
-	    	}catch(Exception e){
-//	    		System.out.println("Something Wrong.");
+	    	if(b==null){
+	    		return a;
 	    	}
-	    	long total = aa+bb;
-	    	System.out.println("aa:"+aa+":bb:"+bb+":total:"+total);
-	    	String totalStr = ""+total;
+	    	String front_number = "";
+	    	String equal_a = "";
+	    	String equal_b = "";
+	    	if(a.length()>b.length()){
+	    		front_number = a.substring(0, a.length()-b.length());
+	    		equal_a = a.substring(a.length()-b.length(),a.length());
+	    		equal_b = b;
+	    	}else{
+	    		front_number = b.substring(0, b.length()-a.length());
+	    		equal_a = a;
+	    		equal_b = b.substring(b.length()-a.length(),b.length());
+	    	}
 	    	boolean up = false;
-	    	StringBuilder sb = new StringBuilder();
-	    	sb.append(totalStr);
-	    	//remember string is from left to right, int is reverse.
-	    	for(int i=sb.length()-1;i>=0;i--){
+	    	StringBuilder sb_e_a = new StringBuilder();
+	    	sb_e_a.append(equal_a);
+	    	StringBuilder sb_e_b = new StringBuilder();
+	    	sb_e_b.append(equal_b);
+
+	    	for(int i=sb_e_a.length()-1;i>=0;i--){
 	    		if(up){
-		    		if(sb.charAt(i)=='0'){
-		    			sb.setCharAt(i, '1');
+		    		if(addTwoChar(sb_e_a.charAt(i),sb_e_b.charAt(i))==0){
+		    			sb_e_a.setCharAt(i, '1');
 		    			up = false;
-		    		}else if(sb.charAt(i)=='1'){
-		    			sb.setCharAt(i, '0');
+		    		}else if(addTwoChar(sb_e_a.charAt(i),sb_e_b.charAt(i))==1){
+		    			sb_e_a.setCharAt(i, '0');
 		    			up = true;
-		    		}else if(sb.charAt(i)=='2'){
-		    			sb.setCharAt(i, '1');
+		    		}else if(addTwoChar(sb_e_a.charAt(i),sb_e_b.charAt(i))==2){
+		    			sb_e_a.setCharAt(i, '1');
 		    			up = true;
 		    		}
 	    		}else{
-	    			if(sb.charAt(i)=='2'){
-		    			sb.setCharAt(i, '0');
+	    			if(addTwoChar(sb_e_a.charAt(i),sb_e_b.charAt(i))==0){
+		    			sb_e_a.setCharAt(i, '0');
+		    			up = false;
+		    		}else if(addTwoChar(sb_e_a.charAt(i),sb_e_b.charAt(i))==1){
+		    			sb_e_a.setCharAt(i, '1');
+		    			up = false;
+		    		}else if(addTwoChar(sb_e_a.charAt(i),sb_e_b.charAt(i))==2){
+	    				sb_e_a.setCharAt(i, '0');
 		    			up = true;
 		    		}
 	    		}
 	    	}
 	    	if(up){
-	    		return "1"+sb.toString();
+	    		StringBuilder sb_front = new StringBuilder();
+	    		sb_front.append(front_number);
+	    		for(int i=sb_front.length()-1;i>=0;i--){
+	    			if(up){
+			    		if(sb_front.charAt(i)=='0'){
+			    			sb_front.setCharAt(i, '1');
+			    			up = false;
+			    		}else if(sb_front.charAt(i)=='1'){
+			    			sb_front.setCharAt(i, '0');
+			    			up = true;
+			    		}else if(sb_front.charAt(i)=='2'){
+			    			sb_front.setCharAt(i, '1');
+			    			up = true;
+			    		}
+		    		}else{
+		    			if(sb_front.charAt(i)=='2'){
+		    				sb_front.setCharAt(i, '0');
+			    			up = true;
+			    		}
+		    		}
+	    		}
+	    		if(up){
+	    			return "1"+sb_front.toString()+sb_e_a.toString();
+	    		}else{
+	    			return sb_front.toString()+sb_e_a.toString();
+	    		}
 	    	}else{
-	    		return sb.toString();
+	    		return front_number+sb_e_a.toString();
 	    	}	        
+	    }
+	    
+	    private int addTwoChar(char a, char b){
+	    	int aa = 0;
+	    	int bb = 0;
+	    	try{
+	    		aa = Character.getNumericValue(a);
+	    	}catch(Exception e){	    		
+	    	}
+	    	try{
+	    		bb = Character.getNumericValue(b);
+	    	}catch(Exception e){	    		
+	    	}
+	    	return aa+bb;
 	    }
 	}
 }
