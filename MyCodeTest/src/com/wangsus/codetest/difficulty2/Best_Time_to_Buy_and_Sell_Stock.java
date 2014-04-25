6d8f29427bb86642dd2af25c11f6fcd8ce85e51d
Su Wang <wangsu@wangsus.com>
2014-04-24 22:30:20 -0400

    Best_Time_to_Buy_and_Sell_Stock Time Limit Exceeded
    n^2 cannot pass

--- MyCodeTest/src/com/wangsus/codetest/difficulty2/Best_Time_to_Buy_and_Sell_Stock.java
+++ MyCodeTest/src/com/wangsus/codetest/difficulty2/Best_Time_to_Buy_and_Sell_Stock.java
@@ -3,12 +3,21 @@
  */
 package com.wangsus.codetest.difficulty2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Su
  *
- *Say you have an array for which the ith element is the price of a given stock on day i.
-
-If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ *         Say you have an array for which the ith element is the price of a
+ *         given stock on day i.
+ * 
+ *         If you were only permitted to complete at most one transaction (ie,
+ *         buy one and sell one share of the stock), design an algorithm to find
+ *         the maximum profit.
  *
  */
 public class Best_Time_to_Buy_and_Sell_Stock {
@@ -20,9 +29,23 @@ public class Best_Time_to_Buy_and_Sell_Stock {
 		// TODO Auto-generated method stub
 
 	}
+
 	public class Solution {
-	    public int maxProfit(int[] prices) {
-	        return 0;
-	    }
+		public int maxProfit(int[] prices) {
+			// it allow sell first then buy it ? maybe not.
+			//
+			if (prices == null || prices.length == 0) {
+				return 0;
+			}
+			int profit = 0;
+			for (int i = 0; i < prices.length; i++) {
+				for (int j = i + 1; j < prices.length; j++) {
+					if (prices[j] - prices[i] - profit > 0) {
+						profit = prices[j] - prices[i];
+					}
+				}
+			}
+			return profit;
+		}
 	}
 }
